0a1b8a82eccda91e2c026aa8d20bb51baef7eb2e
Dondla Sreenivasulu <137170226+Seenu-Dondla@users.noreply.github.com>
2023-06-21 13:15:33 +0530

    Add files via upload

---
+++ Calculator/calc.js
@@ -0,0 +1,12 @@
+function appendValue(value){
+    document.getElementById("result").value += value;
+}
+
+function calculate(){
+    var result = document.getElementById("result").value;
+    var answer = eval(result);
+    document.getElementById("result").value = answer;
+}
+function clearResult(){
+    document.getElementById("result").value = "";
+}
